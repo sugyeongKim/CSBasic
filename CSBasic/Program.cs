cdfbdfd0e2c9efca279d36736895ddbd1de88ba3
sugyeong Kim <s2018s25@e-mirim.hs.kr>
2020-04-29 14:09:30 +0900

    C# 기본 연습

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -10,6 +10,34 @@ namespace CSBasic
     {
         static void Main(string[] args)
         {
+            //273;
+            var name = "김" + "수" + "경";
+            Console.Write("Hello C# ");
+            int add = 2;
+            int Alpha;
+            //주석
+            /*
+             * 여러줄주석
+             */
+
+            Console.WriteLine("Hello World ");
+            Console.Write("HEllo World ");
+            Console.Write("HEllo World ");
+
+            Console.Write(52);
+            Console.WriteLine(7 % 3);
+            Console.WriteLine('A');
+            Console.WriteLine("A");
+
+            Console.WriteLine("한빛\t아카데미");
+            Console.WriteLine("한빛\n아카데미");
+            Console.WriteLine("\"\"\"");
+            Console.WriteLine("가" + "나" + "다");
+
+            Console.WriteLine("안녕"[100]);
+
+            Console.WriteLine(true);
+            Console.WriteLine(true);
         }
     }
 }
